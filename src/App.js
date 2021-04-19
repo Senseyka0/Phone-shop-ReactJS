af3476cf3040492d11ab93c978437be83ea5101b
senseyka <senseyika@gmail.com>
2021-04-19 12:23:24 +0300

    added json-server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
+import axios from "axios";
 
-import DB from "./assets/db.json";
+// import DB from "./assets/db.json";
 
 import Footer from "./components/Footer/Footer";
 import Goods from "./components/Goods/Goods";
@@ -10,78 +11,107 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 import ProductPage from "./components/Goods/ProductPage/ProductPage";
 import Cart from "./components/Cart/Cart";
-
-import "./App.scss";
 import Checkout from "./components/Checkout/Checkout";
 
+import "./App.scss";
+
 const App = () => {
-  const allGoods = DB.goods.iphone.concat(DB.goods.mac, DB.goods.ipad);
+   const [goods, setGoods] = useState([]);
+   const [isLoaded, setIsLoaded] = useState(false);
 
-  const [addedToCartProduct, setAddedToCartProduct] = useState([]);
+   const allGoods = isLoaded && goods.iphone.concat(goods.mac, goods.ipad);
 
-  const addToCartProduct = (product) => {
-    setAddedToCartProduct([...addedToCartProduct, product]);
-  };
-  return (
-    <Router>
-      <div className="App">
-        <div className="container">
-          <Header cartProducts={addedToCartProduct} />
+   useEffect(() => {
+      setIsLoaded(false);
+      axios.get("/goods").then(({ data }) => {
+         setGoods(data);
+         setIsLoaded(true);
+      });
+   }, []);
 
-          <h1 className="store">Senseyka's Store.</h1>
+   const [addedToCartProduct, setAddedToCartProduct] = useState([]);
 
-          <div className="content">
-            <Sidebar />
-            <Route exact path="/" render={() => <Goods goods={allGoods} title="All" />} />
-            <Route
-              exact
-              path="/cart"
-              render={() => <Cart cartProducts={addedToCartProduct} />}
-            />
-            <Route
-              exact
-              path={`/products/:productName?`}
-              render={(e) => (
-                <ProductPage
-                  route={e}
-                  goods={allGoods}
-                  addedToCartProduct={addedToCartProduct}
-                  addToCartProduct={addToCartProduct}
-                />
-              )}
-            />
-            <Route
-              path="/checkout"
-              render={() => <Checkout cartProducts={addedToCartProduct} />}
-            />
+   const addToCartProduct = (product) => {
+      setAddedToCartProduct([...addedToCartProduct, product]);
+   };
 
-            <Route
-              exact
-              path="/iphones"
-              render={() => <Goods goods={DB.goods.iphone} title="Iphone" />}
-            />
-            <Route
-              path="/mac"
-              render={() => <Goods goods={DB.goods.mac} title="Mac" />}
-            />
-            <Route
-              path="/ipad"
-              render={() => <Goods goods={DB.goods.ipad} title="Ipad" />}
-            />
-            <Route
-              path="/watch"
-              render={() => <Goods goods={DB.goods.watch} title="Watch" />}
-            />
-            <Route
-              path="/accessories"
-              render={() => <Goods goods={DB.goods.accessories} title="Accessories" />}
-            />
-          </div>
-          <Footer />
-        </div>
-      </div>
-    </Router>
-  );
+   return (
+      <Router>
+         <div className="App">
+            <div className="container">
+               <Header cartProducts={addedToCartProduct} />
+
+               <h1 className="store">Alex's Store.</h1>
+
+               <div className="content">
+                  <Sidebar />
+                  {isLoaded && (
+                     <Route
+                        exact
+                        path="/"
+                        render={() => <Goods goods={allGoods} title="All" />}
+                     />
+                  )}
+                  <Route
+                     exact
+                     path="/cart"
+                     render={() => <Cart cartProducts={addedToCartProduct} />}
+                  />
+                  {isLoaded && (
+                     <Route
+                        exact
+                        path={`/products/:productName?`}
+                        render={(e) => (
+                           <ProductPage
+                              route={e}
+                              goods={allGoods}
+                              addedToCartProduct={addedToCartProduct}
+                              addToCartProduct={addToCartProduct}
+                           />
+                        )}
+                     />
+                  )}
+                  <Route
+                     path="/checkout"
+                     render={() => <Checkout cartProducts={addedToCartProduct} />}
+                  />
+                  {isLoaded && (
+                     <Route
+                        exact
+                        path="/iphones"
+                        render={() => <Goods goods={goods.iphone} title="Iphone" />}
+                     />
+                  )}
+                  {isLoaded && (
+                     <Route
+                        path="/mac"
+                        render={() => <Goods goods={goods.mac} title="Mac" />}
+                     />
+                  )}
+                  {isLoaded && (
+                     <Route
+                        path="/ipad"
+                        render={() => <Goods goods={goods.ipad} title="Ipad" />}
+                     />
+                  )}
+                  {isLoaded && (
+                     <Route
+                        path="/watch"
+                        render={() => <Goods goods={goods.watch} title="Watch" />}
+                     />
+                  )}
+                  {isLoaded && (
+                     <Route
+                        path="/accessories"
+                        render={() => <Goods goods={goods.accessories} title="Accessories" />}
+                     />
+                  )}
+               </div>
+               <Footer />
+            </div>
+         </div>
+      </Router>
+   );
 };
 
 export default App;
--- src/components/Goods/Goods.jsx
+++ src/components/Goods/Goods.jsx
@@ -3,18 +3,18 @@ import CardItem from "./CardItem/CardItem";
 
 import "./Goods.scss";
 
-function Goods(props) {
-  return (
-    <div className="goods">
-      <h1 className="goods__title">{props.title}</h1>
-      <div className="goods-items">
-        {props.goods !== undefined &&
-          props.goods.map((card) => {
-            return <CardItem key={card.name} cardItem={card} />;
-          })}
+function Goods({ goods, title }) {
+   return (
+      <div className="goods">
+         <h1 className="goods__title">{title}</h1>
+         <div className="goods-items">
+            {goods !== undefined &&
+               goods.map((card) => {
+                  return <CardItem key={card.name} cardItem={card} />;
+               })}
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default Goods;
