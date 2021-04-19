6fd5701e15c10c069811e92bce076bf83d5795c9
senseyka <senseyika@gmail.com>
2021-04-19 12:38:36 +0300

    minor-changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,38 +8,36 @@ import { SiVisa } from "react-icons/si";
 import "./Footer.scss";
 
 function Footer() {
-  return (
-    <div className="footer">
-      <div className="footer-top">
-        <div className="footer-top__links">
-          <p>Links</p>
-          <ul>
-            <NavLink to="/about">About us</NavLink>
-            <NavLink to="/news">News</NavLink>
-            <NavLink to="/contact">Contact Us</NavLink>
-          </ul>
-        </div>
-        <div className="footer-top__subscribe">
-          <p>Be modern</p>
-          <div className="footer-subscribe__email">
-            <p>Promotions, new products and sales. Directly to your inbox</p>
-            <input type="email" placeholder="Your email" />
-            <button>Subscribe</button>
-          </div>
-        </div>
+   return (
+      <div className="footer">
+         <div className="footer-top">
+            <div className="footer-top__links">
+               <p>Links</p>
+               <ul>
+                  <NavLink to="/about">About us</NavLink>
+                  <NavLink to="/news">News</NavLink>
+                  <NavLink to="/contact">Contact Us</NavLink>
+               </ul>
+            </div>
+            <div className="footer-top__subscribe">
+               <p>Be modern</p>
+               <div className="footer-subscribe__email">
+                  <p>Promotions, new products and sales. Directly to your inbox</p>
+                  <input type="email" placeholder="Your email" />
+                  <button>Subscribe</button>
+               </div>
+            </div>
+         </div>
+         <div className="footer-bottom">
+            <div className="footer-bottom__copyright">Copyright © 2021 AlexM</div>
+            <div className="footer-bottom__banks">
+               <SiVisa />
+               <RiMastercardLine />
+               <RiPaypalLine />
+            </div>
+         </div>
       </div>
-      <div className="footer-bottom">
-        <div className="footer-bottom__copyright">
-          Copyright © 2020 Senseyka
-        </div>
-        <div className="footer-bottom__banks">
-          <SiVisa />
-          <RiMastercardLine />
-          <RiPaypalLine />
-        </div>
-      </div>
-    </div>
-  );
+   );
 }
 
 export default Footer;
